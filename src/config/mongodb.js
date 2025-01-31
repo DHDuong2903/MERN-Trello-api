61c675400e5f2602992b6612c950cd4ed34a2b6b
DHDuong2903 <dohuyduong2903@gmail.com>
2025-01-31 16:33:14 +0700

    handle env file

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,19 +1,9 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
-const MONGODB_URI =
-  "mongodb+srv://duongdo:DM1BGqeqRMLzJhB3@cluster0-duongdo.m6x26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-DuongDo";
-
-const DATABASE_NAME = "MERN-Trello";
-
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "./environment";
 
 let trelloDatabaseInstance = null;
 
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -25,7 +15,7 @@ const mongoClientInstance = new MongoClient(MONGODB_URI, {
 export const CONNECT_DB = async () => {
   await mongoClientInstance.connect();
 
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
 };
 
 // Dong ket noi toi DB khi can
