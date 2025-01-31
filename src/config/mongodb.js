b8f2e018331aa78646bd57c57ff1eb132aa172d8
DHDuong2903 <dohuyduong2903@gmail.com>
2025-01-31 16:07:51 +0700

    Handle close connect database

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,37 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+const MONGODB_URI =
+  "mongodb+srv://duongdo:DM1BGqeqRMLzJhB3@cluster0-duongdo.m6x26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-DuongDo";
+
+const DATABASE_NAME = "MERN-Trello";
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+let trelloDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Ket noi toi DB
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+
+// Dong ket noi toi DB khi can
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error("Must connect to Database first!");
+  return trelloDatabaseInstance;
+};
