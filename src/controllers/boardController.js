7e0cc6e51f091eb2de6ffede0f20dcf3bb783acc
DHDuong2903 <dohuyduong2903@gmail.com>
2025-02-20 22:12:07 +0700

    add_column_card

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -3,10 +3,6 @@ import { boardService } from "~/services/boardService";
 
 const createNew = async (req, res, next) => {
   try {
-    // console.log("req.body: ", req.body);
-    // console.log("req.query: ", req.query);
-    // console.log("req.params: ", req.params);
-
     //  Dieu huong du lieu sang tang Service
     const createdBoard = await boardService.createNew(req.body);
 
@@ -19,7 +15,6 @@ const createNew = async (req, res, next) => {
 
 const getDetails = async (req, res, next) => {
   try {
-    // console.log("req.params: ", req.params);
     const boardId = req.params.id;
 
     const board = await boardService.getDetails(boardId);
@@ -30,7 +25,19 @@ const getDetails = async (req, res, next) => {
   }
 };
 
+const update = async (req, res, next) => {
+  try {
+    const boardId = req.params.id;
+    const updatedBoard = await boardService.update(boardId, req.body);
+
+    res.status(StatusCodes.OK).json(updatedBoard);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const boardController = {
   createNew,
   getDetails,
+  update,
 };
--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -22,6 +22,8 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false),
 });
 
+const INVALID_UPDATE_FEILDS = ["_id", "createdAt"];
+
 const validateBeforeCreate = async (data) => {
   return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false });
 };
@@ -101,7 +103,24 @@ const pushColumnOrderIds = async (column) => {
         { returnDocument: "after" }
       );
 
-    return result.value;
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const update = async (boardId, updateData) => {
+  try {
+    Object.keys(updateData).forEach((fieldName) => {
+      if (INVALID_UPDATE_FEILDS.includes(fieldName)) {
+        delete updateData[fieldName];
+      }
+    });
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOneAndUpdate({ _id: new ObjectId(boardId) }, { $set: updateData }, { returnDocument: "after" });
+
+    return result;
   } catch (error) {
     throw new Error(error);
   }
@@ -114,4 +133,5 @@ export const boardModel = {
   findOneById,
   getDetails,
   pushColumnOrderIds,
+  update,
 };
