2dfa5a448936045181b87fc6b291da6cc4fbcc08
DHDuong2903 <dohuyduong2903@gmail.com>
2025-02-23 21:45:50 +0700

    update_trello_api_to_deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 // Nhung domain duoc phep truy cap toi tai nguyen cua server
-export const WHITELIST_DOMAINS = [
-  // "http://localhost:5173"
-];
+export const WHITELIST_DOMAINS = ["https://mern-trello-web.vercel.app/"];
 
 export const BOARD_TYPES = {
   PUBLIC: "public",
