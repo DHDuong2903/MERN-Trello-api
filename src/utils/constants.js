d1b5998747535fbbc16e80a8322b2213f07f3988
DHDuong2903 <dohuyduong2903@gmail.com>
2025-02-23 16:47:43 +0700

    update_CORS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 // Nhung domain duoc phep truy cap toi tai nguyen cua server
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+export const WHITELIST_DOMAINS = [
+  // "http://localhost:5173"
+];
 
 export const BOARD_TYPES = {
   PUBLIC: "public",
